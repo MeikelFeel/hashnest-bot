bf3b99b9391fd29c22e048f9340825af5bef8ed6
astrolince <astro@astrolince.com>
2017-08-04 17:03:23 -0300

    hashrate stats

--- bot.py
+++ bot.py
@@ -156,7 +156,7 @@ accusdvalue=btcusd * accbtcvalue + ltcusd * accltcvalue
 gettoday = json.loads(db.get(today).decode('utf-8')) or accusdvalue
 meanaccusdvalue = mean([float(gettoday[0] or accusdvalue), accusdvalue])
 
-getyesterday = json.loads(db.get(today - timedelta(days=1)).decode('utf-8') or [meanaccusdvalue, s7hashrate, s9hashrate, l3hashrate])
+getyesterday = json.loads(db.get(today - timedelta(days=1)).decode('utf-8')) or [meanaccusdvalue, s7hashrate, s9hashrate, l3hashrate]
 getweek = json.loads(db.get(today - timedelta(days=7)).decode('utf-8') or [accusdvalue, s7hashrate, s9hashrate, l3hashrate])
 getmonth = json.loads(db.get(today - timedelta(days=30)) or [accusdvalue, s7hashrate, s9hashrate, l3hashrate])
 getyear = json.loads(db.get(today - timedelta(days=365)) or [accusdvalue, s7hashrate, s9hashrate, l3hashrate])
