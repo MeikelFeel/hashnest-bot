52291119f532b296d5c99479e6a4aac76308f704
astrolince <astro@astrolince.com>
2017-08-24 07:49:43 -0300

    profit maintenance percent calc and db set

--- bot.py
+++ bot.py
@@ -36,11 +36,11 @@ poolData = json.loads(requests.get('https://www.antpool.com/webService.htm').tex
 btcNetworkDiff = str(poolData['networkDiff']).translate({ord(c): None for c in ','})
 ltcNetworkDiff = str(poolData['ltcNetworkDiff']).translate({ord(c): None for c in ','})
 
-btcGains=antpoolCalculator('BTC', btcNetworkDiff)
-ltcGains=antpoolCalculator('LTC', ltcNetworkDiff)
+btcgains=antpoolCalculator('BTC', btcNetworkDiff)
+ltcgains=antpoolCalculator('LTC', ltcNetworkDiff)
 
-db.set('btcGains', btcGains)
-db.set('ltcGains', ltcGains)
+db.set('btcgains', btcgains)
+db.set('ltcgains', ltcgains)
 
 #hashnest_api = hashnest('username', 'access_key', 'secret_key')
 hashnest_api = hashnest(os.environ['username'], os.environ['access_key'], os.environ['secret_key'])
@@ -184,6 +184,13 @@ accusdvalue=btcusd * accbtcvalue + ltcusd * accltcvalue
 s7tradesmedianusd = s7tradesmedian * btcusd
 s9tradesmedianusd = s9tradesmedian * btcusd
 l3tradesmedianusd = l3tradesmedian * ltcusd
+
+s7maintenancepercent=0.41/(btcgains*btcusd)
+s9maintenancepercent=0.19/(btcgains*btcusd)
+l3maintenancepercent=2.7/(ltcgains*ltcusd)
+db.set('s7maintenancepercent', s7maintenancepercent)
+db.set('s9maintenancepercent', s9maintenancepercent)
+db.set('l3maintenancepercent', l3maintenancepercent)
 
 try:
     gettoday = json.loads(db.get(today).decode('utf-8'))
